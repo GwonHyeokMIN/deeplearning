67ce44c9687ada79218d888310fb5d89ba779ec6
GwonHyeokMIN <hgha1194990@gmail.com>
2021-05-06 15:34:57 +0900

    ch01

---
+++ image_show.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+img = imread('/home/hgha1194990/deep-learning-from-scratch/dataset/cactus.png')
+plt.imshow(img)
+
+plt.show()
